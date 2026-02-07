4ced0901a28b730fe10b50c4632560fc5faa3c48
Arkapravo Rajkonwar <arkapravocollegemail@gmail.com>
2026-02-07 12:00:00 +0530

    auto-commit for 4d7318ff-879e-40aa-8a04-e63109533114

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useListingStore } from '../store/listingStore';
 import SearchBar from '../components/listings/SearchBar';
 import FilterModal from '../components/listings/FilterModal';

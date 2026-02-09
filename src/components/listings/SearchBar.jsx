679e15a29315bd43d723b425105fb532a516e05c
Arkapravo Rajkonwar <arkapravocollegemail@gmail.com>
2026-02-09 12:00:00 +0530

    auto-commit for e66c2bc1-e51e-43eb-b28c-5798143176fb

--- src/components/listings/SearchBar.jsx
+++ src/components/listings/SearchBar.jsx
@@ -37,7 +37,7 @@ export default function SearchBar() {
   };
   
   const customSelectStyles = {
-    control: (provided, state) => ({
+    control: (provided) => ({
       ...provided,
       border: 'none',
       boxShadow: 'none',
